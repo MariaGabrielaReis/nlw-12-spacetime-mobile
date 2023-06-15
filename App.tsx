01bed6634cf90103c9fa8bf32759b4822a2f8596
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-06-14 21:26:10 -0300

    chore: configurate fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) return null;
+
   return (
-    <View className="bg-zinc-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 text-4xl">Hi!</Text>
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className="text-gray-50 text-4xl">Hi!</Text>
       <StatusBar style="light" translucent />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 module.exports = {
   content: ["./App.tsx", "./app/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: "Roboto_700Bold",
+        body: "Roboto_400Regular",
+        alt: "BaiJamjuree_700Bold",
+      },
+
+      colors: {
+        gray: {
+          50: "#eaeaea",
+          100: "#bebebf",
+          200: "#9e9ea0",
+          300: "#727275",
+          400: "#56565a",
+          500: "#2c2c31",
+          600: "#28282d",
+          700: "#1f1f23",
+          800: "#18181b",
+          900: "#121215",
+        },
+        purple: {
+          50: "#F3EEFC",
+          100: "#D8CBF7",
+          200: "#C6B2F3",
+          300: "#AB8EEE",
+          400: "#9B79EA",
+          500: "#8257E5",
+          600: "#764FD0",
+          700: "#5C3EA3",
+          800: "#48307E",
+          900: "#372560",
+        },
+        orange: {
+          50: "#FFF7ED",
+          100: "#FFEDD5",
+          200: "#FED7AA",
+          300: "#FDBA74",
+          400: "#FB923C",
+          500: "#F97316",
+          600: "#EA580C",
+          700: "#C2410C",
+          800: "#9A3412",
+          900: "#7C2D12",
+        },
+      },
+    },
   },
   plugins: [],
 };
